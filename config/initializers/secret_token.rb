5cb9c7161e49d24f039d21607462a91a4bd5f244
Rubens Souza <r_henrique@yahoo.com>
2014-01-18 19:41:32 -0200

    update user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minierp::Application.config.secret_key_base = '8aa1f207e4c8a6f4d2e77716f9f03ba870982538abce8012f8e783ab6e6d4cfd8f758a47a08e5b18ffbe9a91057ef9946025cc45c42aad0f47b342b437e58f8f'
